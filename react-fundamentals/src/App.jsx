84e802bbc3efbea05c57d6e937085bfcc9a47b94
Marcus Elliott <melliott@pop-os.localdomain>
2021-08-13 19:59:19 -0400

    latest Blue badge Files -8/13/2021 @ 8PM

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -5,7 +5,6 @@ import "./App.css";
 import Header from "./components/site/Header";
 import Footer from "./components/site/Footer";
 import Sidebar from "./components/site/Sidebar";
-
 import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
--- react-fundamentals/src/components/concepts/FunctionalComponentDemo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FunctionalComponentDemo = function () {
-  return <div>Hello React</div>;
-};
-
-export default FunctionalComponentDemo;
